26f27403dc732379824b87ae4f4b6c251e741aa1
ayadav44237 <79020880+ayadav44237@users.noreply.github.com>
2022-03-23 21:50:45 -0800

    Time: 103 ms (60.03%), Space: 41.9 MB (57.42%) - LeetHub

---
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int ans=0;
+        
+     
+             int low=0,high=people.size()-1;
+            while(low<=high){
+                if(people[low]+people[high]<=limit){
+                    ++low;
+                    --high;
+                }
+                else{
+                    --high;
+                }
+                 ++ans;
+            
+           
+        }
+        return ans;
+    }
+};
