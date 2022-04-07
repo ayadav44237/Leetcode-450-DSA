b1db9b7ef4aa17483f9a61e6939b2f524f61a35b
ayadav44237 <79020880+ayadav44237@users.noreply.github.com>
2022-04-07 02:14:12 -0800

    Time: 9 ms (26.98%), Space: 9.6 MB (72.76%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int low=0,high=nums.size()-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]<target){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
-        }
-        return low;
+        // int low=0,high=nums.size()-1;
+        // while(low<=high){
+        //     int mid=low+(high-low)/2;//here high-low to overcome stack overflow in case of large values...this code will also work in case of duplicates..
+        //     if(nums[mid]<target){
+        //         low=mid+1;
+        //     }
+        //     else{
+        //         high=mid-1;
+        //     }
+        // }
+        // return low;
+//         APPROACH 2;
+        return lower_bound(nums.begin(),nums.end(),target)-nums.begin();
     }
 };
